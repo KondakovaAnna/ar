f36097b2d278595defc523c454fe05483f1e548d
KondakovaAnna <you@example.com>
2024-09-22 10:12:32 +0300

    laba number one 1.1

---
+++ app/src/main/java/no/realitylab/arface/MainActivity.kt
@@ -0,0 +1,17 @@
+package no.realitylab.arface
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, GlassesActivity::class.java))
+
+
+    }
+
+}
